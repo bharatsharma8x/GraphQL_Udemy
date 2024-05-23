54d9cbf86c82ef5c85bed44286a13919245d374c
bsharma <bsharma@technomatz.com>
2024-05-23 20:52:01 +0530

    (14) Active Record Fields

--- app/graphql/types/author_type.rb
+++ app/graphql/types/author_type.rb
@@ -9,5 +9,10 @@ class Types::AuthorType < Types::BaseObject
     field :is_alive, Boolean, null: false
     # field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     # field :created_at, String, null: false
+    field :full_name, String, null: true
+
+    def full_name
+        "#{object.first_name} #{object.last_name}"
+    end
 
 end
