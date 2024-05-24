b8056393a7c7500c773c587c66461900177f1497
bsharma <bsharma@technomatz.com>
2024-05-24 14:19:24 +0530

    (21) Input types

--- app/graphql/types/author_type.rb
+++ app/graphql/types/author_type.rb
@@ -18,3 +18,15 @@ class Types::AuthorType < Types::BaseObject
     end
 
 end
+
+class Types::AuthorInputType < GraphQL::Schema::InputObject
+
+    graphql_name "AuthorInputType"
+    description "All the attributes needed to create an author"
+
+    argument :first_name, String, required: false
+    argument :last_name, String, required: false
+    argument :yob, Int, required: false
+    argument :is_alive, Boolean, required: false
+
+end
