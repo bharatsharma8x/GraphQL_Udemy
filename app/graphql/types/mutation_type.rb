1933c31c34b1c2dfab46d3a28005b7c3a96dd365
bsharma <bsharma@technomatz.com>
2024-05-24 15:50:38 +0530

    (24) Delete record

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -9,9 +9,17 @@ module Types
       argument :author, Types::AuthorInputType, required: true
     end
 
+    field :delete_author, Boolean, null: false, description: "Delete the Author" do 
+      argument :id, ID, required: true
+    end
+
     def update_author(author:)
       existing_author = Author.find(author[:id])
       existing_author&.update author.to_h
+    end
+
+    def delete_author(id:)
+      Author.find(id).destroy
     end
 
   end
